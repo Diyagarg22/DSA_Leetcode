ca4dfcb5702a76d3c2416a07ba8568fe918b16e8
Diyagarg22 <diya.garg_cs24@gla.ac.in>
2026-09-18 09:53:31 +0530

    Time: 1 ms (99.89%), Space: 47.2 MB (97.74%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,15 +1,20 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        
         int n=nums.length;
-        int i=0;
-        for(int j=0;j<n;j++){
-            if(nums[j]!=0){
-              int temp=nums[i];
-              nums[i]=nums[j];
-              nums[j]=temp;
-              i++;
+        int j=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]!=0){
+                nums[j]=nums[i];
+                j++;
+
             }
+
+
         }
+        while(j<n){
+            nums[j]=0;
+            j++;
+        }
+        
     }
 }
